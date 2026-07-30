cace79c9239844a75a372051ede07f9d3740e689
Syed40A <72885427+Syed40H@users.noreply.github.com>
2026-07-30 06:15:19 -0400

    learning

--- Arrays-Hashing/VAnagram.java
+++ Arrays-Hashing/VAnagram.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
             return false;
@@ -22,6 +22,24 @@ class Solution {
             charCounts.put(c, count - 1);
         }
 
+        return true;
+    }
+}
+*/
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int [] arr = new int [26];
+
+        for(int i=0; i <s.length(); i++){
+            arr[s.charAt(i)-'a']++;
+        }
+         for(int i=0; i <t.length(); i++){
+            arr[t.charAt(i)-'a']--;
+            if(arr[t.charAt(i)-'a'] < 0)
+            return false;
+        }
         return true;
     }
 }
